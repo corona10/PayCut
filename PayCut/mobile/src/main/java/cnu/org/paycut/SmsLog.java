fca978e8f47d948c65a42b58506fe80098b094c4
Dong-hee Na <corona10@gmail.com>
2016-05-01 23:01:13 +0900

    reformat

--- PayCut/mobile/src/main/java/cnu/org/paycut/SmsLog.java
+++ PayCut/mobile/src/main/java/cnu/org/paycut/SmsLog.java
@@ -4,6 +4,6 @@ package cnu.org.paycut;
  * Created by corona10 on 2016. 5. 1..
  */
 public class SmsLog {
-    private float price;
-    private String seller;
+  private float price;
+  private String seller;
 }
